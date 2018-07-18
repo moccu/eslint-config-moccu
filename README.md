6bf38082340dbe74c63b2a6f1c9c02cdc7b77bee
Sascha Künstler <sascha.kuenstler@moccu.com>
2018-07-18 15:24:38 +0200

    #1 Prepare test

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # eslint-config-moccu
+
 The eslint config we use at moccu
+
+## Installation
+
+```
+$ npm install --save-dev eslint-config-moccu
+```
+
+## Usage
+
+```js
+{
+  "extends": "moccu",
+  "rules": {
+    // Additional rules...
+  }
+}
+```
+
+## License
+
+[MIT](./LICENSE)
---
+++ test/test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const
+	assert = require('assert'),
+	eslint = require('eslint'),
+	conf = require('../'),
+	files = ['**/*.js'],
+	report = new eslint.CLIEngine({
+		useEslintrc: false,
+		envs: [
+			'node','es6'
+		],
+		rules: conf.rules
+	}).executeOnFiles(files)
+;
+
+// Shor error report
+// eslint.CLIEngine.getErrorResults(report.results).forEach((error) => {
+// 	console.log(error.messages);
+// });
+
+// Self test
+assert.equal(report.errorCount, 0);
+assert.equal(report.warningCount, 0);
