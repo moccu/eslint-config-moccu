0ecea523d5411a66ddb1c62314ca60c1cede8d5c
Martin Schneider <martin.schneider@moccu.com>
2019-01-23 12:35:09 +0100

    split react rules in default and proptypes and remove unused rules

---
+++ react-proptypes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+	plugins: [
+		'react'
+	],
+	rules: {
+		'react/boolean-prop-naming': [2, {'rule': '^(is|has|can)[A-Z]([A-Za-z0-9]?)+'}],
+		'react/no-unused-prop-types': [2],
+		'react/require-default-props': [2],
+		'react/sort-prop-types': [2, {
+			'callbacksLast': true,
+			'ignoreCase': true,
+			'requiredFirst': true,
+			'sortShapeProp': true,
+			'noSortAlphabetically': false
+		}]
+	}
+};
