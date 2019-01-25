eaaef860609e5ce9ba1069d4de5b921be51a6420
Norman Rusch <norman.rusch@moccu.com>
2019-01-25 09:54:34 +0100

    Update List helper to clone itself on insert() call
    This prevents to unexpectedly change default values

--- helper/index.js
+++ helper/index.js
@@ -3,27 +3,29 @@
 module.exports.List = class extends Array {
 
 	insert(value) {
+		const clone = this.concat();
+
 		return {
 			after: (element) => {
-				let index = this.indexOf(element);
+				let index = clone.indexOf(element);
 
 				if (index < 0) {
-					index = this.length - 1;
+					index = clone.length - 1;
 				}
 
-				this.splice(index + 1, 0, value);
-				return this;
+				clone.splice(index + 1, 0, value);
+				return clone;
 			},
 
 			before: (element) => {
-				let index = this.indexOf(element);
+				let index = clone.indexOf(element);
 
 				if (index < 0) {
 					index = 0;
 				}
 
-				this.splice(index, 0, value);
-				return this;
+				clone.splice(index, 0, value);
+				return clone;
 			}
 		};
 	}
--- test/helper.js
+++ test/helper.js
@@ -8,50 +8,59 @@ const
 
 
 (function () {
-	const list = new List(1, 2, 3);
+	const
+		list = new List(1, 2, 3),
+		clone = list.insert('a').before(3)
+	;
 
-	assert.equal(list.insert('a').before(3), list);
-	assert.deepEqual(list, [1, 2, 'a', 3]);
+	assert.notEqual(clone, list);
+	assert.deepEqual(clone, [1, 2, 'a', 3]);
 })();
 
 
 (function () {
 	const list = new List(1, 2, 3);
 
-	list
-		.insert('a').before(1)
-		.insert('b').before(42);
+	assert.deepEqual(
+		list
+			.insert('a').before(1)
+			.insert('b').before(42),
+		['b', 'a', 1, 2, 3]
+	);
+})();
 
-	assert.deepEqual(list, ['b', 'a', 1, 2, 3]);
+
+(function () {
+	const
+		list = new List(1, 2, 3),
+		clone = list.insert('a').after(3)
+	;
+
+	assert.notEqual(clone, list);
+	assert.deepEqual(clone, [1, 2, 3, 'a']);
 })();
 
 
 (function () {
 	const list = new List(1, 2, 3);
 
-	assert.equal(list.insert('a').after(3), list);
-	assert.deepEqual(list, [1, 2, 3, 'a']);
+	assert.deepEqual(
+		list
+			.insert('a').after(1)
+			.insert('b').after(42),
+		[1, 'a', 2, 3, 'b']
+	);
 })();
 
 
 (function () {
 	const list = new List(1, 2, 3);
 
-	list
-		.insert('a').after(1)
-		.insert('b').after(42);
-
-	assert.deepEqual(list, [1, 'a', 2, 3, 'b']);
-})();
-
-
-(function () {
-	const list = new List(1, 2, 3);
-
-	list
-		.insert('a').before(1)
-		.insert('c').after('a')
-		.insert('b').before('c');
-
-	assert.deepEqual(list, ['a', 'b', 'c', 1, 2, 3]);
+	assert.deepEqual(
+		list
+			.insert('a').before(1)
+			.insert('c').after('a')
+			.insert('b').before('c'),
+		['a', 'b', 'c', 1, 2, 3]
+	);
 })();
