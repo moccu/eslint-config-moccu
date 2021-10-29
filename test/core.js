1fdc1331e62ac1374dd07801c97ce56315f02dd2
Sascha Künstler <sascha.kuenstler@moccu.com>
2021-10-29 10:56:24 +0200

    Update tests

--- test/core.js
+++ test/core.js
@@ -1,33 +1,52 @@
 'use strict';
 
-
-const
-	assert = require('assert'),
-	chalk = require('chalk'),
-	deepmerge = require('deepmerge'),
-	eslint = require('eslint'),
-	files = ['**/*.js'],
-	config = ['index', 'react', 'react-proptypes'].reduce(
-		(cfg, name) => deepmerge(cfg, require('../' + name)),
-		{
+(async function() {
+	const
+		assert = require('assert'),
+		chalk = require('chalk'),
+		deepmerge = require('deepmerge'),
+		{ESLint} = require('eslint'),
+		files = ['**/*.js'],
+		eslint = new ESLint({
 			useEslintrc: false,
-			envs: ['node', 'es6']
+			overrideConfig: {
+				env: {
+					'es6': true,
+					'node': true
+				},
+				parserOptions: {
+					'ecmaVersion': 9
+				},
+				...['index', 'react', 'react-proptypes'].reduce(
+					(cfg, name) => deepmerge(cfg, require('../' + name))
+				)
+			}
 		}),
-	report = new eslint.CLIEngine(config).executeOnFiles(files);
-;
+		results = await eslint.lintFiles(files)
+	;
 
+	let
+		errorCount = 0,
+		warningCount = 0
+	;
 
-// Show error report
-eslint.CLIEngine.getErrorResults(report.results).forEach((error) =>
-	error.messages.forEach((message) =>
-		console.error(
-			chalk.red(message.message),
-			chalk.white(`(${message.ruleId})`),
-			chalk.white(`\n\t${error.filePath}:${message.line}:${message.column}`)
+	// Show error report
+	ESLint.getErrorResults(results).forEach((error) => {
+		errorCount += error.errorCount;
+		warningCount += error.warningCount;
+		error.messages.forEach((message) =>
+			console.error(
+				chalk.red(message.message),
+				chalk.white(`(${message.ruleId})`),
+				chalk.white(`\n\t${error.filePath}:${message.line}:${message.column}`)
+			)
 		)
-	)
-);
+	});
 
-// Self test
-assert.equal(report.errorCount, 0);
-assert.equal(report.warningCount, 0);
+	// Self test
+	assert.equal(errorCount, 0);
+	assert.equal(warningCount, 0);
+})().catch((error) => {
+	process.exitCode = 1;
+	console.error(error);
+});
